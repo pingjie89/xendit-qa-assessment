c085ac2ab25e0221523bff56a9836046b2ac08ce
unknown <pingjie.ng@outlook.com>
2020-09-12 21:09:41 +0800

    Use image pixel as comparison for result, Add some sample expectedImage that create from trial run

--- features/calculator.feature
+++ features/calculator.feature
@@ -1,12 +1,47 @@
 Feature: Test online calculator scenarios
-Scenario Outline: Test subtraction, division and CE functionalities
-Given Open chrome browser and start application
-When I enter following values and press CE button
-			|value1 | <value1>|
-			|value2 | <value2>|
-			|operator | <operator>|			
-Then I should be able to see
-			|	expected |<expected>|
-Examples:
-		| value1  		| value2 		| operator			| expected	|
-    	| 	2 			|   2			|		/			| 1			|
+	Scenario Outline: Simple Two Value Operation
+	Given Open chrome browser and start application
+	When I enter following values and press = button
+				|value1 	| <value1>	|	
+				|operator 	| <operator>|	
+				|value2 	| <value2>	|
+	Then I should be able to see
+				|expected 	| <expected>|
+	Examples:
+			| value1	| value2	| operator	| expected	|
+			| 	23		|   10	 	|		*	| 230		|
+			| 	1 		|   1	 	|		+	| 2			|
+			| 	20 		|   20	 	|		-	| 0			|
+			| 	64 		|   8	 	|		/	| 8			|
+
+	Scenario Outline: Single Value Operation
+	Given Open chrome browser and start application
+	When I enter single value with operator only button
+				|value1 	| <value1>	|	
+				|operator 	| <operator>|	
+	Then I should be able to see
+				|expected 	| <expected>|
+	Examples:
+			| value1	| operator	| expected	|
+			| 	4		|		R	| 2  		|
+			| 	123123	|		C	| 0CE		|
+			| 	2 		|		R	| 1.41421356|
+			| 	10 		|		%	| 0.1		|
+			| 	20		|		#	| -20		|
+
+# KeyMapping - Missing 1/x :(
+# sqrt | R
+# MC | V
+# M+ | B
+# M- | N
+# MR | M
+# C  | C
+# %  | %
+# /  | /
+# +  | +
+# -  | -
+# =  | =
+# x  | *
+# .  | .
+# #  | -/+
+
--- step-definitions/calculator-steps.js
+++ step-definitions/calculator-steps.js
@@ -1,93 +1,137 @@
 //const expect = require('chai').expect;
+const { fail } = require('assert');
 
-var webdriver=require('selenium-webdriver'),
+const webdriver=require('selenium-webdriver'),
 By = webdriver.By,
 until = webdriver.until;
-var imagePath;
 
 module.exports = function () {
- this.Given(/^Open chrome browser and start application$/, function () {
-        return driver.get('https://www.online-calculator.com/full-screen-calculator/');
-       });
-  this.When(/^I enter following values and press CE button$/, function (dataTable) {
-        var strValue1=dataTable.raw()[0][1];
-        var strValue2=dataTable.raw()[1][1];
-        var strOperator=dataTable.raw()[2][1];
-      });
-this.Then(/^I should be able to see$/, function (table) {
-       
-       });
 
-function takeScreenshot() {
+    this.Given(/^Open chrome browser and start application$/, async function () {
+        await driver.get('https://www.online-calculator.com/html5/online-calculator/index.php');
+    });
+
+    this.When(/^I enter following values and press = button$/, async function (dataTable) {
+        
+        sendKey(dataTable.raw()[0][1]);
+        sendKey(dataTable.raw()[1][1]);
+        sendKey(dataTable.raw()[2][1]);
+        sendKey('=');
+
+    });
+
+    this.When(/^I enter single value with operator only button$/, async function (dataTable) {
+        
+        sendKey(dataTable.raw()[0][1]);
+        sendKey(dataTable.raw()[1][1]);
+        sendKey('=');
+
+    });
+
+    this.Then(/^I should be able to see$/, async function (table) {
+       var expectedValue = table.raw()[0][1];
+       await new Promise(r => setTimeout(r, 2000));
+       var returnImagePath = takeScreenshot(expectedValue);
+       await new Promise(r => setTimeout(r, 2000));
+       if(!imageCompare(returnImagePath, expectedValue)) { fail('Image return does not match with expectedImage folder images') };
+    });
+
+    this.After(async function() {
+        //await driver.Close();
+    });
+
+    function sendKey(values) {
+        driver.findElement(By.xpath('//body')).sendKeys(values);
+    }
+
+    function imageCompare(ImagePathForVerification, expectedValue) {
+        const fs = require('fs');
+        const PNG = require('pngjs').PNG;
+        const pixelmatch = require('pixelmatch');
+        
+        const img1 = PNG.sync.read(fs.readFileSync("./expectedImage/" + expectedValue + ".tiff"));
+        const img2 = PNG.sync.read(fs.readFileSync(ImagePathForVerification));
+        const {width, height} = img1;
+        const diff = new PNG({width, height});
+        
+        var numberDiff = pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.1});
+        //fs.writeFileSync('./unexpectedResult/expectingValue'+ expectedValue  +'diff.png', PNG.sync.write(diff));
+        process.stdout.write("Pixel different: "+ numberDiff);
+        return numberDiff > 0 ? false : true; //should be identical
+    }
+
+    function takeScreenshot(expectedValue) {
     const fs = require("fs");
     var now = new Date().getTime();
-   imagePath="./reports/"+now+".tiff";
-   webElement=driver.findElement(By.id("canvas"));
-    var base64Data = "";
-    var location = {};
-    var bulk = [];
-    driver.then(_ => {
-        webElement.getLocation().then(e => {
-            location.x = e.x;
-            location.y = e.y;
-        });
-        webElement.getSize().then(e => {
-            location.height = e.height;
-            location.width = e.width;
-        });
-        driver.manage().window().getSize().then(e => {
-            location.browserHeight = e.height;
-            location.broserWidth = e.width;
-        });
-    }).then(_ => {
-        driver.takeScreenshot().then(data => {
-            base64Data = data.replace(/^data:image\/png;base64,/, "");
-        });
-    }).then(_ => {
-        const sizeLimit = 700000; // around 700kb
-        const imgSize = base64Data.length;
-        driver.executeScript(() => {
-            window.temp = new Array;
+    var imagePath="./reports/"+now+"_"+expectedValue+".tiff";
+    webElement=driver.findElement(By.id("canvas"));
+        var base64Data = "";
+        var location = {};
+        var bulk = [];
+        driver.then(_ => {
+            webElement.getLocation().then(e => {
+                location.x = e.x;
+                location.y = e.y;
+            });
+            webElement.getSize().then(e => {
+                location.height = e.height;
+                location.width = e.width;
+            });
+            driver.manage().window().getSize().then(e => {
+                location.browserHeight = e.height;
+                location.broserWidth = e.width;
+            });
         }).then(_ => {
-            for (var i = 0; i < imgSize; i += sizeLimit) {
-                bulk.push(base64Data.substring(i, i + sizeLimit));
-            }
-            bulk.forEach((element, index) => {
-                driver.executeScript(() => {
-                    window.temp[arguments[0]] = arguments[1];
-                }, index, element);
+            driver.takeScreenshot().then(data => {
+                base64Data = data.replace(/^data:image\/png;base64,/, "");
+            });
+        }).then(_ => {
+            const sizeLimit = 700000; // around 700kb
+            const imgSize = base64Data.length;
+            driver.executeScript(() => {
+                window.temp = new Array;
+            }).then(_ => {
+                for (var i = 0; i < imgSize; i += sizeLimit) {
+                    bulk.push(base64Data.substring(i, i + sizeLimit));
+                }
+                bulk.forEach((element, index) => {
+                    driver.executeScript(() => {
+                        window.temp[arguments[0]] = arguments[1];
+                    }, index, element);
+                });
+            });
+        }).then(_ => {
+            driver.executeScript(() => {
+                var tempBase64 = window.temp.join("");
+                var image = new Image();
+                var location = arguments[0];
+                image.src = "data:image/png;base64," + tempBase64;
+                image.onload = function () {
+                    var canvas = document.createElement("canvas");
+                    canvas.height = location.height;
+                    canvas.width = location.width;
+                    canvas.style.height = location.height + 'px';
+                    canvas.style.width  = location.width + 'px';
+                    var ctx = canvas.getContext('2d');
+                    ctx.drawImage(image, -location.x, -location.y);
+                    window.canvasData = canvas.toDataURL();
+                    window.temp = [];
+                }
+            }, location);
+        }).then(_ => {
+            return driver.executeScript(() => {
+                var data = window.canvasData;
+                window.canvasData = "";
+                return data;
+            }).then(data => {
+                var tempData = data.replace(/^data:image\/png;base64,/, "");
+                fs.writeFileSync(imagePath, tempData, "base64");
             });
         });
-    }).then(_ => {
-        driver.executeScript(() => {
-            var tempBase64 = window.temp.join("");
-            var image = new Image();
-            var location = arguments[0];
-            image.src = "data:image/png;base64," + tempBase64;
-            image.onload = function () {
-                var canvas = document.createElement("canvas");
-                canvas.height = location.height;
-                canvas.width = location.width;
-                canvas.style.height = location.height + 'px';
-                canvas.style.width  = location.width + 'px';
-                var ctx = canvas.getContext('2d');
-                ctx.drawImage(image, -location.x, -location.y);
-                window.canvasData = canvas.toDataURL();
-                window.temp = [];
-            }
-        }, location);
-    }).then(_ => {
-        return driver.executeScript(() => {
-            var data = window.canvasData;
-            window.canvasData = "";
-            return data;
-        }).then(data => {
-            var tempData = data.replace(/^data:image\/png;base64,/, "");
-            fs.writeFileSync(imagePath, tempData, "base64");
-        });
-    });
-}
 
-function verify_calculatedAmount(localFilePath) { 
-}
+        return imagePath;
+    }
+
+    function verify_calculatedAmount(localFilePath) { 
+    }
 };
